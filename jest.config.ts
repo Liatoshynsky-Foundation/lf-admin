7098667e7ebe71305edf65417d16aa8658ac7b91
yur4uwe <YURII.TSAR@lnu.edu.ua>
2025-07-01 20:50:48 +0300

    feat: test commit 4th try to run husky is a success

--- jest.config.ts
+++ jest.config.ts
@@ -1,33 +1,50 @@
 import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './'
+});
 
 const config: Config = {
   bail: 1,
   collectCoverage: true,
   collectCoverageFrom: [
     'app/**/*.{js,jsx,ts,tsx}',
-    '!app/**/*.test.{js,jsx,ts,tsx}', // Exclude test files from coverage
+    '!app/**/*.test.{js,jsx,ts,tsx}',
+    '!app/**/*.{types,d}.{ts,tsx}',
+    '!app/constants/**',
+    '!app/types/**'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
-    },
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
   },
-  coverageReporters: ['text', 'lcov','json', 'html'],
+  coverageReporters: ['text', 'lcov', 'json', 'html'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^~/public/(.*)$': '<rootDir>/public/$1',
+    '^~/i18n/(.*)$': '<rootDir>/i18n/$1',
+    '^~/utils/(.*)$': '<rootDir>/app/lib/utils/$1',
+    '^~/ds-components/(.*)$': '<rootDir>/app/shared/components/design-system/all-components/$1',
+    '^~/components/(.*)$': '<rootDir>/app/shared/components/$1',
+    '^~/hooks/(.*)$': '<rootDir>/app/shared/hooks/$1',
+    '^~/(.*)$': '<rootDir>/app/$1'
+  },
   modulePaths: ['<rootDir>/app'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest'
   },
   transformIgnorePatterns: ['node_modules/(?!(lodash-es)/)'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom']
 };
 
-export default config;
+export default createJestConfig(config);
