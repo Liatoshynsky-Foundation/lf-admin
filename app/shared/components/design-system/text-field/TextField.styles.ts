6e33358dc0b81950f245e98d267d0638f1991a59
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-03 18:23:48 +0300

    fix: update styles

--- app/shared/components/design-system/text-field/TextField.styles.ts
+++ app/shared/components/design-system/text-field/TextField.styles.ts
@@ -9,23 +9,28 @@ export const styles = {
       borderWidth: '1.5px'
     },
     '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
-      borderColor: '#aaa'
-    },
-    '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
-      borderColor: '#bbb'
+      borderColor: '#190d033d'
     },
 
-    '& .MuiOutlinedInput-root.Mui-focused': {
-      boxShadow: 'none'
+    '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#d3d3d3'
     },
 
     '& .MuiInputLabel-root': {
-      fontSize: 12,
-      color: '#888'
+      fontSize: 12
+    },
+
+    '& .MuiOutlinedInput-root:hover .MuiOutlinedInput-notchedOutline': {
+      borderColor: '#d3d3d3'
     },
     '& .MuiInputLabel-shrink': {
       transform: 'translate(14px, -6px) scale(0.85)'
     },
+
+    '& .MuiInputLabel-root.Mui-focused': {
+      color: '#52545a'
+    },
+
     '& input': {
       padding: '12px 16px',
       fontSize: 16,
