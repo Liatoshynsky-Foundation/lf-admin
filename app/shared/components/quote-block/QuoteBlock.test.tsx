23a269e0803769f27fa8b48707257edadfbab4b5
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-07-17 08:50:21 +0300

    fix: update QouteBlock component

--- app/shared/components/quote-block/QuoteBlock.test.tsx
+++ app/shared/components/quote-block/QuoteBlock.test.tsx
@@ -5,13 +5,17 @@ import { QuoteBlock } from './QuoteBlock';
 
 describe('QuoteBlock', () => {
   it('should render both text fields with correct titles and default values', () => {
-    render(<QuoteBlock />);
+    render(
+      <QuoteBlock
+        defaultMainQuote="Буде, звісно, дуже багато цікавого, але всього не почуєш"
+        defaultCaption="Лист Бориса Лятошинського Маргариті Царевич"
+      />
+    );
 
-    expect(screen.getByText('Головна цитата')).toBeInTheDocument();
     expect(screen.getByText('Підпис до цитати')).toBeInTheDocument();
 
-    expect(screen.getByDisplayValue(/Буде, звісно, дуже багато цікавого, але всього не почуєш/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue('Буде, звісно, дуже багато цікавого, але всього не почуєш')).toBeInTheDocument();
 
-    expect(screen.getByDisplayValue(/Лист Бориса Лятошинського Маргариті Царевич/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue('Лист Бориса Лятошинського Маргариті Царевич')).toBeInTheDocument();
   });
 });
--- app/shared/components/quote-block/QuoteBlock.tsx
+++ app/shared/components/quote-block/QuoteBlock.tsx
@@ -3,11 +3,22 @@ import React from 'react';
 
 import { CustomTextField } from '../design-system/text-field/TextField';
 
-export const QuoteBlock: React.FC = () => {
+type QuoteBlockProps = {
+  defaultMainQuote?: string;
+  defaultCaption?: string;
+};
+
+export const QuoteBlock = ({ defaultMainQuote, defaultCaption }: QuoteBlockProps) => {
   return (
     <Box display="flex" flexDirection="column" gap="16px">
-      <CustomTextField title="Головна цитата" label="Текст підпису" fullWidth multiline />
-      <CustomTextField title="Підпис до цитати" label="Текст підпису" fullWidth />
+      <CustomTextField
+        title="Головна цитата"
+        label="Текст підпису"
+        defaultValue={defaultMainQuote}
+        fullWidth
+        multiline
+      />
+      <CustomTextField title="Підпис до цитати" label="Текст підпису" defaultValue={defaultCaption} fullWidth />
     </Box>
   );
 };
