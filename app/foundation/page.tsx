3339442049d21fd425d45326da1709eb57cc3511
Iarynovskyi <dimayar20061@gmail.com>
2025-07-17 19:22:22 +0300

    fix: delete foundation page

--- app/foundation/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { LiatoshynskyOffice } from '~/components/accordion-blocks/Liatoshynsky-office/Liatoshynsky-office';
-
-export default async function Foundation() {
-  return <LiatoshynskyOffice />;
-}
