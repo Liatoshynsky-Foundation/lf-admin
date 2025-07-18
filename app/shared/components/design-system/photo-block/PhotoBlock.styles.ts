2f95ef362088505750c5bee2a1493207ba909236
olegchaplia <156513251+oleg191006@users.noreply.github.com>
2025-07-18 12:20:00 +0300

    Implement photo block component (#114)
    * feat: add photo block component
    
    * refactor: replace MUI Button with custom Button
    
    * chore: update imports to use alias

---
+++ app/shared/components/design-system/photo-block/PhotoBlock.styles.ts
@@ -0,0 +1,36 @@
+export const styles = {
+  container: {
+    p: 2,
+    maxWidth: '1050px'
+  },
+
+  sectionTitle: {
+    fontSize: '18px',
+    fontWeight: 500,
+    color: 'rgba(71, 77, 90, 1)',
+    mb: 1,
+    fontFamily: 'Mulish'
+  },
+
+  imageBlock: {
+    display: 'flex',
+    gap: 2,
+    alignItems: 'flex-start'
+  },
+
+  imagePreview: {
+    maxWidth: '500px',
+    maxHeight: '300px'
+  },
+
+  fileNameText: {
+    color: 'rgba(71, 77, 90, 1)',
+    fontFamily: 'Mulish',
+    mb: '8px'
+  },
+
+  imageSizeText: {
+    mb: 4,
+    fontFamily: 'Mulish'
+  }
+};
