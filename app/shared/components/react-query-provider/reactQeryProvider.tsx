e91ae0c8d0cb5f3e82d3c1cdcde1c2a74b94a088
Iarynovskyi <dimayar20061@gmail.com>
2025-07-22 19:55:01 +0300

    fix: sonar issue

--- app/shared/components/react-query-provider/reactQeryProvider.tsx
+++ app/shared/components/react-query-provider/reactQeryProvider.tsx
@@ -3,7 +3,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 
-export function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+export function ReactQueryProvider({ children }: Readonly<{ children: React.ReactNode }>) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
