7c16e9c11ca4f98475dc7ae75286f8aa44e65055
Iarynovskyi <dimayar20061@gmail.com>
2025-07-29 20:15:04 +0300

    fix: sonar issue

--- app/shared/components/apollo-client-provider/apolloClientProvider.tsx
+++ app/shared/components/apollo-client-provider/apolloClientProvider.tsx
@@ -4,6 +4,6 @@ import { ApolloProvider } from '@apollo/client';
 
 import { client } from '~/lib/utils/apollo-client';
 
-export function ApolloClientProvider({ children }: { children: React.ReactNode }) {
+export function ApolloClientProvider({ children }: Readonly<{ children: React.ReactNode }>) {
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 }
