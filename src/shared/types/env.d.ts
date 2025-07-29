d494aba62b39b547ebf504cef3e29383037a8edc
Iarynovskyi Dmytro <164883382+Iarynovskyi@users.noreply.github.com>
2025-07-29 11:53:08 +0300

    (SP: 0,5) Rename jwt tokens in env file (#146)
    * fix: naming and add types for env
    
    * fix: forgot change mock

---
+++ src/shared/types/env.d.ts
@@ -0,0 +1,13 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production';
+    MONGO_USERNAME: string;
+    MONGO_PASSWORD: string;
+    MONGO_DB: string;
+    MONGO_HOST: string;
+    MONGO_PORT: number;
+    AZURE_SAS_URL: string;
+    JWT_ACCESS_TOKEN_SECRET: string;
+    JWT_REFRESH_TOKEN_SECRET: string;
+  }
+}
