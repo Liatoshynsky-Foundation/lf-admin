5f94628545bb5a619a70d433a74c3607676a525a
nika <vvvveronikaaaa666@gmail.com>
2025-07-29 17:15:47 +0300

    removed card from page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <>
     <h1>Liatoshynsky project</h1>;
-    <ContributorCard contributorNameValue={''} contributorDescriptionValue={''}/>
     </>
   )
   
