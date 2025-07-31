6863be9182b3b7f6fd423f91ce37e3c75dbd0b9c
nika <vvvveronikaaaa666@gmail.com>
2025-07-31 11:26:10 +0300

    add zustand configs

--- app/(logged_id)/page.tsx
+++ app/(logged_id)/page.tsx
@@ -1,3 +1,9 @@
+<<<<<<< HEAD:app/(logged_id)/page.tsx
+=======
+import React from 'react';
+
+import { ContributorCard } from './shared/components/contributor-card/ContributorCard';
+>>>>>>> 0991a67 (add zustand configs):app/page.tsx
 export default function Home() {
-  return <h1>Liatoshynsky project</h1>;
+  return <ContributorCard contributorNameValue={''} contributorDescriptionValue={''} />;
 }
