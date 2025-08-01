765dad252db77af1416d0c5a1bef25a4a096aa24
nika <vvvveronikaaaa666@gmail.com>
2025-08-01 12:21:02 +0300

    removed comonent from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,4 @@
 import React from 'react';
-
-import { ContributorCard } from './shared/components/contributor-card/ContributorCard';
 export default function Home() {
-  return (
-    <>
-      <h1>Liatoshynsky project</h1>
-      <ContributorCard contributorNameValue={''} contributorDescriptionValue={''} />
-    </>
-  );
+  return <h1>Liatoshynsky project</h1>;
 }
