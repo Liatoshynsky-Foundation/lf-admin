403f6d460181945d214441754c87409695b6d7b7
Vitaliy Kormylo <vitaliykormylo@gmail.com>
2025-08-14 19:37:02 +0300

    feat: implemented functionality for preview button

--- app/(logged_id)/layout.tsx
+++ app/(logged_id)/layout.tsx
@@ -14,7 +14,7 @@ export default async function RootLayout({
   return (
     <BodyProvider>
       <SideBarNavigation />
-      <Container maxWidth="md" sx={{ border: '1px solid #ccc', padding: '20px' }}>
+      <Container maxWidth="md" sx={{ padding: '20px' }}>
         {children}
       </Container>
     </BodyProvider>
