fbc3d2cfc0a70148899a20751159786bed3b0fae
Iarynovskyi <dimayar20061@gmail.com>
2025-08-15 15:34:01 +0300

    fix: sonar issue

--- migrations/template.ts
+++ migrations/template.ts
@@ -1,10 +1,10 @@
 import type { Connection } from 'mongoose';
 
 export async function up(_connection: Connection): Promise<void> {
-  // TODO write your migration here.
+  // Write your migration here.
   // See https://github.com/seppevs/migrate-ts for examples.
 }
 
 export async function down(_connection: Connection): Promise<void> {
-  // TODO write the statements to rollback your migration (if possible)
+  // Write the statements to rollback your migration (if possible)
 }
