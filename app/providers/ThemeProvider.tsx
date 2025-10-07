18ba371f5a966b5d1c3fd5cc8e53f5e7db1ca218
bohuslavstan <50991998+bohuslavstan@users.noreply.github.com>
2025-10-07 14:05:51 +0300

    Remove comment from ThemeProvider component
    Removed comment about theme instance creation.

--- app/providers/ThemeProvider.tsx
+++ app/providers/ThemeProvider.tsx
@@ -5,7 +5,6 @@ import React, { PropsWithChildren, useMemo } from 'react';
 import { createAdminTheme } from '../shared/theme/theme';
 
 export const ThemeProvider = ({ children }: PropsWithChildren) => {
-  // Create theme instance client-side to avoid serialization issues
   const theme = useMemo(() => createAdminTheme(), []);
 
   return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
