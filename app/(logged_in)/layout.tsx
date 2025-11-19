541256e95180decc71cbcdd7e269fab3a2131612
yur4uwe <YURII.TSAR@lnu.edu.ua>
2025-11-19 20:35:10 +0200

    fix: sonnar

--- app/(logged_in)/layout.tsx
+++ app/(logged_in)/layout.tsx
@@ -1,11 +1,11 @@
 import '../globals.css';
 import { Container } from '@mui/material';
 
-import BodyProvider, { metadata as LayoutMetadata } from '~/providers/body-provider/BodyProvider';
+import BodyProvider from '~/providers/body-provider/BodyProvider';
 import { SideBarNavigation } from '~/shared/components/side-navigation/SideNavigation';
 import DiscardModalProvider from '~/shared/providers/discard-modal-provider/DiscardModalProvider';
 
-export const metadata = LayoutMetadata;
+export { metadata } from '~/providers/body-provider/BodyProvider';
 
 export default async function RootLayout({
   children
