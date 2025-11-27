3e58403782debfeeabf15cb285e5b8e7f1eb7b9d
Anastasia <105491259+qqwz0@users.noreply.github.com>
2025-11-27 15:41:57 +0200

    fix(ui): fix input autofill wrong color (#332)

--- app/shared/components/design-system/text-field/TextField.styles.ts
+++ app/shared/components/design-system/text-field/TextField.styles.ts
@@ -35,6 +35,14 @@ export const styles = {
       padding: '12px 16px',
       fontSize: 16,
       color: '#000'
+    },
+    '& .MuiInputLabel-root[data-shrink="false"] ~ .MuiInputBase-root input': {
+      opacity: 0,
+      transition: 'opacity 0.2s ease'
+    },
+    '& .MuiInputLabel-root[data-shrink="true"] ~ .MuiInputBase-root input': {
+      opacity: 1,
+      transition: 'opacity 0.2s ease'
     }
   },
   titleStyles: {
