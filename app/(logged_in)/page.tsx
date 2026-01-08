849b573e7ab45809074162080c3702d2643f0d68
Ruslan Symonenko <symonenkors@gmail.com>
2026-01-08 18:48:08 +0200

    chore: comment tests while component is not ready

--- app/(logged_in)/page.tsx
+++ app/(logged_in)/page.tsx
@@ -4,7 +4,6 @@ import { Box } from '@mui/material';
 import React from 'react';
 
 import { fetchPreview } from '~/lib/utils/fetchPreview';
-import { BlockNoteEditorExample } from '~/shared/components/block-note-editor/BlockNoteEditorExample';
 import { Header } from '~/shared/components/header/Header';
 import { useStore } from '~/store';
 
@@ -37,7 +36,6 @@ export default function Home() {
         onCancel={onCancel}
         isSaving
       />
-      <BlockNoteEditorExample />
     </Box>
   );
 }
