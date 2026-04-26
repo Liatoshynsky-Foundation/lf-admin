20b9d442a01e5a45ac8079ed8f033e77ee0c10c4
Jevgan <skinnycoding@gmail.com>
2026-04-26 13:02:49 +0300

    fix: provide 404 id if type is not correct

--- app/(logged_in)/publications/[type]/[id]/seo/page.tsx
+++ app/(logged_in)/publications/[type]/[id]/seo/page.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import { Box } from '@mui/material';
-import { useParams, useRouter } from 'next/navigation';
+import { notFound, useParams, useRouter } from 'next/navigation';
 
 import CreatePublicationView from '~/(logged_in)/publications/[type]/create/CreatePublicationsView';
-import { PAGE_TITLES, PUBLICATIONS_BASE_PATH, PublicationsItemType } from '~/constants/publications';
+import {
+  PAGE_TITLES,
+  PUBLICATIONS_BASE_PATH,
+  PUBLICATIONS_TYPES,
+  PublicationsItemType
+} from '~/constants/publications';
 import DividedHeader from '~/shared/components/divided-header/DividedHeader';
 import HeaderRightActions from '~/shared/components/divided-header/header-right-actions/HeaderRightActions';
 import { TitleDropdown } from '~/shared/components/divided-header/title-dropdown/TitleDropdown';
@@ -21,6 +26,8 @@ export default function PublicatiosSeoPage() {
 
   const publicationData = useUpsertPublication({ type, id });
 
+  if (!PUBLICATIONS_TYPES.includes(type)) notFound();
+
   const handleCancel = () => router.push(PUBLICATIONS_BASE_PATH);
   const handleSave = () => {
     publicationData?.handleSave();
@@ -30,6 +37,7 @@ export default function PublicatiosSeoPage() {
   return (
     <Box>
       <DividedHeader
+        originUrl={PUBLICATIONS_BASE_PATH}
         rightActionsComponent={<HeaderRightActions mode="seo" onSave={handleSave} onCancel={handleCancel} />}
       >
         <TitleDropdown type="SEO" title={`Редагування ${PAGE_TITLES[type]}`} renderMenuOpen={false} />
