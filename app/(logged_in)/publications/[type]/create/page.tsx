2ff84b8d90867e8112cea7006dacdae8f0d91f70
Jevgan <skinnycoding@gmail.com>
2026-04-28 00:39:11 +0300

    refactor: move DividedHeader with actions mapping to actions menu

--- app/(logged_in)/publications/[type]/create/page.tsx
+++ app/(logged_in)/publications/[type]/create/page.tsx
@@ -1,15 +1,14 @@
 'use client';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { notFound, useParams } from 'next/navigation';
 
 import CreatePublicationsView from './CreatePublicationsView';
 import { styles } from './page.styles';
-import { PAGE_TITLES, PUBLICATIONS_BASE_PATH, PUBLICATIONS_TYPES, PublicationsItemType } from '~/constants/publications';
-import DividedHeader from '~/shared/components/divided-header/DividedHeader';
-import HeaderRightActions from '~/shared/components/divided-header/header-right-actions/HeaderRightActions';
 import {
-  useUpsertPublication,
-} from '~/shared/hooks/use-upsert-publication/useUpsertPublication';
+  PUBLICATIONS_TYPES,
+  PublicationsItemType
+} from '~/constants/publications';
+import { useUpsertPublication } from '~/shared/hooks/use-upsert-publication/useUpsertPublication';
 
 export default function CreatePublicationPage() {
   const params = useParams();
@@ -19,14 +18,9 @@ export default function CreatePublicationPage() {
 
   if (!PUBLICATIONS_TYPES.includes(type)) notFound();
 
+
   return (
     <Box sx={styles.container}>
-      <DividedHeader
-        originUrl={PUBLICATIONS_BASE_PATH}
-        rightActionsComponent={<HeaderRightActions mode="create" onEdit={publicationData.handleSave} />}
-      >
-        <Typography variant="customBold20Tight">{`Створення ${PAGE_TITLES[type]}`}</Typography>
-      </DividedHeader>
 
       <CreatePublicationsView data={publicationData} />
     </Box>
