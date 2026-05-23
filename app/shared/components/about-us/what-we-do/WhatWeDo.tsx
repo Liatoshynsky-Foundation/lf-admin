eadb66379eab2115d8a66d2de8bd9aa4b1bcd97e
Jevgan <skinnycoding@gmail.com>
2026-05-23 14:41:57 +0300

    chore: get rid of ProseDoc types replacing them to JSONContent

--- app/shared/components/about-us/what-we-do/WhatWeDo.tsx
+++ app/shared/components/about-us/what-we-do/WhatWeDo.tsx
@@ -1,10 +1,10 @@
 import { Skeleton } from '@mui/material';
+import { JSONContent} from '@tiptap/react';
 
 import { EditableSectionList, SectionListItem } from '../../accordion-blocks/editable-section-list/EditableSectionList';
 import { BLOCK_IDS, PAGE_IDS } from '~/constants/pageBlocks';
 import CollapsibleBlock from '~/ds-components/collapsible-block/CollapsibleBlock';
 import { ensureIds } from '~/lib/utils/ensureIds';
-import { proseToText, textToProse } from '~/lib/utils/prose';
 import { usePageBlock } from '~/shared/hooks/use-page-block/usePageBlock';
 import { useStore } from '~/store';
 import { LocalizedString } from '~/types/common';
@@ -26,17 +26,17 @@ const WhatWeDo = () => {
   const points: SectionListItem[] = itemList.map((item) => ({
     id: item.id,
     title: item.title[currentLocale],
-    description: proseToText(item.description[currentLocale])
+    description: item.description[currentLocale]
   }));
 
-  const handleTitleChange = (value: string) =>
+  const handleTitleChange = (value: JSONContent) =>
     setField(pageId, blockId, 'title', { ...block.title, [currentLocale]: value });
 
-  const handleChangeItem = (id: string, field: 'title' | 'description', value: string) => {
+  const handleChangeItem = (id: string, field: 'title' | 'description', value: JSONContent) => {
     const updatedList = itemList.map((item) => {
       if (item.id !== id) return item;
 
-      const updatedFieldValue = field === 'description' ? textToProse(value) : value;
+      const updatedFieldValue = field === 'description' ? value : value;
       return { ...item, [field]: { ...item[field], [currentLocale]: updatedFieldValue } };
     });
     setField(pageId, blockId, 'items', updatedList);
@@ -45,11 +45,11 @@ const WhatWeDo = () => {
   const handleCreateItem = (): SectionListItem => {
     const newItem: WhatWeDolItemWithId = {
       id: crypto.randomUUID(),
-      title: { uk: '', en: '' },
+      title: { uk: {}, en: {} },
       description: { uk: { type: 'doc', content: [] }, en: { type: 'doc', content: [] } }
     };
     setField(pageId, blockId, 'items', [...itemList, newItem]);
-    return { id: newItem.id, title: '', description: '' };
+    return { id: newItem.id, title: {}, description: {} };
   };
 
   const handleDeleteItem = (id: string) =>
