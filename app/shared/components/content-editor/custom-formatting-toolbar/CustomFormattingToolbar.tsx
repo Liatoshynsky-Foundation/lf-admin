7714258bd8e86b198365b16ce8a4d7f9c5ae6a7e
tl <andrewzhubinskiy21@gmail.com>
2026-06-26 16:07:31 +0300

    feat: apply new filtering logic for items & add tests

--- app/shared/components/content-editor/custom-formatting-toolbar/CustomFormattingToolbar.tsx
+++ app/shared/components/content-editor/custom-formatting-toolbar/CustomFormattingToolbar.tsx
@@ -1,4 +1,4 @@
-import {  FormattingToolbar, getFormattingToolbarItems } from '@blocknote/react';
+import { blockTypeSelectItems, FormattingToolbar, getFormattingToolbarItems, useBlockNoteEditor } from '@blocknote/react';
 
 import { MediaModalResult } from '../../media-modal/MediaModal.types';
 import { CustomReplaceButton } from '../custom-replace-button/CustomReplaceButton';
@@ -7,12 +7,21 @@ interface Props {
 }
 
 export const CustomFormattingToolbar = ({ openMediaModal }: Props) => {
-  const items = getFormattingToolbarItems();
-  const replaceIndex = items.findIndex((item) => item.key === 'replaceFileButton');
+  const editor = useBlockNoteEditor();
+  const defaultDropdownItems = blockTypeSelectItems(editor.dictionary);
+
+  const customizedDropdownItems = defaultDropdownItems.filter((it) =>
+    !(it.type === 'heading' && it.name.includes('Toggle Heading')) && it.type !== 'quote' && it.type !== 'toggleListItem'
+  );
+
+  const mainToolbarItems = getFormattingToolbarItems(customizedDropdownItems);
+  const filteredMainToolbarItems = mainToolbarItems.filter((it) => it.key !== 'strikeStyleButton' && it.key !== 'colorStyleButton' && it.key !== 'nestBlockButton' && it.key !== 'unnestBlockButton');
+
+  const replaceIndex = filteredMainToolbarItems.findIndex((item) => item.key === 'replaceFileButton');
 
   if (replaceIndex !== -1) {
-    items.splice(replaceIndex, 1, <CustomReplaceButton key="customReplaceButton" openMediaModal={openMediaModal} />);
+    filteredMainToolbarItems.splice(replaceIndex, 1, <CustomReplaceButton key="customReplaceButton" openMediaModal={openMediaModal} />);
   }
 
-  return <FormattingToolbar>{items}</FormattingToolbar>;
+  return <FormattingToolbar>{filteredMainToolbarItems}</FormattingToolbar>;
 };
