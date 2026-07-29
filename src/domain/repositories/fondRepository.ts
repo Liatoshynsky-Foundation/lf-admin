ceaae7691423b503e20b2f84fac978146afe461e
tl <andrewzhubinskiy21@gmail.com>
2026-07-29 13:59:12 +0300

    feat(resolvers): added both update and delete methods

--- src/domain/repositories/fondRepository.ts
+++ src/domain/repositories/fondRepository.ts
@@ -4,8 +4,10 @@ import { FiltersInput, IBaseRepository } from './baseRepository';
 export type FondFilters = FiltersInput
 
 export type CreateFondInput = Omit<Fond, 'id' | 'createdAt' | 'updatedAt'>
+export type UpdateFondInput = Omit<Fond, 'id' | 'createdAt' | 'updatedAt'>
 
 export type IFondRepository = IBaseRepository<Fond, FondFilters> & {
     create(input: CreateFondInput): Promise<Fond>;
+    update(id: Fond['id'], input: UpdateFondInput): Promise<Fond>;
     findByFondNumber(fondNumber: Fond['fondNumber']): Promise<Fond | null>
 }
--- src/interfaces/graphql/resolvers/fond/fondMutation.ts
+++ src/interfaces/graphql/resolvers/fond/fondMutation.ts
@@ -2,7 +2,7 @@ import { GraphQLError } from 'graphql';
 
 import { FondErrorCodes, FondErrors } from '~/constants/errors';
 import { Fond } from '~/src/domain/entities/Fond';
-import { CreateFondInput } from '~/src/domain/repositories/fondRepository';
+import { CreateFondInput, UpdateFondInput } from '~/src/domain/repositories/fondRepository';
 import { GraphQLContext } from '~/src/shared/types/container/types';
 import { zFondSchema } from '~/src/validators/fond.schema';
 
@@ -23,5 +23,28 @@ export const FondMutation = {
     }
 
     return repo.create(validatedInput);
+  },
+
+  updateFond: async (_: unknown, { id, input }: { id: string, input: UpdateFondInput }, context: GraphQLContext): Promise<Fond> => {
+    const repo = context.requestContainer.cradle.fondRepository;
+
+    const validateInput = zFondSchema.parse(input);
+    const updatedFond = await repo.update(id, validateInput);
+
+    if (!updatedFond) {
+      throw new GraphQLError(FondErrors.FOND_NOT_FOUND(id), {
+        extensions: {
+          code: FondErrorCodes.FOND_NOT_FOUND
+        }
+      });
+    }
+
+    return updatedFond;
+  },
+
+  deleteFond: async (_: unknown, { id }: { id: string }, context: GraphQLContext): Promise<boolean> => {
+    const repo = context.requestContainer.cradle.fondRepository;
+
+    return await repo.delete(id);
   }
 };
