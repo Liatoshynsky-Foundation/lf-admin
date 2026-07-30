f7eadbd64f49edd8834a824a23aa4b2b7cf67fd4
tl <andrewzhubinskiy21@gmail.com>
2026-07-30 09:02:15 +0300

    test: added tests for fondMutation methods

---
+++ src/interfaces/graphql/resolvers/fond/fondMutation.test.ts
@@ -0,0 +1,152 @@
+import { GraphQLError } from 'graphql';
+import { ZodError } from 'zod';
+
+import { createMockContext } from '../testUtils';
+import { FondMutation } from './fondMutation';
+import { CreateFondInput, IFondRepository, UpdateFondInput } from '~/src/domain/repositories/fondRepository';
+import { BaseContentStatuses } from '~/types/enums/common.enums';
+
+const createMockCreateFondInput = (overrides: Partial<CreateFondInput> = {}): CreateFondInput => ({
+  fondNumber: 1,
+  name: { uk: 'Архів', en: 'Archive' },
+  documentCreationDate: { uk: '1917', en: '1917' },
+  chronologicalBoundaries: { uk: '1917–1991', en: '1917–1991' },
+  organizationForm: { uk: 'Державна установа', en: 'State Institution' },
+  description: undefined,
+  status: BaseContentStatuses.Draft,
+  ...overrides
+});
+
+const createMockUpdateFondInput = (overrides: Partial<UpdateFondInput> = {}): UpdateFondInput => ({
+  fondNumber: 1,
+  name: { uk: 'Оновлений Архів', en: 'Updated Archive' },
+  documentCreationDate: { uk: '1918', en: '1918' },
+  chronologicalBoundaries: { uk: '1918–1991', en: '1918–1991' },
+  organizationForm: { uk: 'Приватна установа', en: 'Private Institution' },
+  description: undefined,
+  status: BaseContentStatuses.Published,
+  ...overrides
+});
+
+const mockCreate = jest.fn();
+const mockUpdate = jest.fn();
+const mockDelete = jest.fn();
+const mockFindById = jest.fn();
+const mockFindByFondNumber = jest.fn();
+const mockFindAll = jest.fn();
+
+const mockRepo: Partial<IFondRepository> = {
+  create: mockCreate,
+  update: mockUpdate,
+  delete: mockDelete,
+  findById: mockFindById,
+  findByFondNumber: mockFindByFondNumber,
+  findAll: mockFindAll
+};
+describe('FondMutation', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const adminContext = createMockContext(true, 'fondRepository', mockRepo);
+  const userContext = createMockContext(false, 'fondRepository', mockRepo);
+
+  describe('createFond', () => {
+    it('should throw GraphQLError if user is not an admin', async () => {
+      const input = createMockCreateFondInput();
+      await expect(FondMutation.createFond({}, { input }, userContext)).rejects.toThrow(GraphQLError);
+
+      expect(mockFindByFondNumber).not.toHaveBeenCalled();
+      expect(mockCreate).not.toHaveBeenCalled();
+    });
+
+    it('should throw ZodError if input fails validation', async () => {
+      const invalidInput = createMockCreateFondInput({ fondNumber: -1 }); 
+
+      await expect(FondMutation.createFond({}, { input: invalidInput }, adminContext)).rejects.toThrow(ZodError);
+
+      expect(mockFindByFondNumber).not.toHaveBeenCalled();
+      expect(mockCreate).not.toHaveBeenCalled();
+    });
+
+    it('should throw custom error msg if fond with this fondNumber already exists', async () => {
+      const input = createMockCreateFondInput();
+
+      mockFindByFondNumber.mockResolvedValue(input);
+
+      await expect(FondMutation.createFond({}, { input }, adminContext)).rejects.toThrow(GraphQLError);
+    });
+
+    it('should successfully call repo create method and return new fond instance', async () => {
+      const input = createMockCreateFondInput();
+
+      mockFindByFondNumber.mockResolvedValue(null);
+
+      await FondMutation.createFond({}, { input }, adminContext);
+
+      expect(mockCreate).toHaveBeenCalledTimes(1);
+      expect(mockCreate).toHaveBeenCalledWith(input);
+    });
+  });
+
+  describe('updateFond', () => {
+    it('should throw GraphQLError if user is not an admin', async () => {
+      const update = createMockUpdateFondInput();
+      await expect(FondMutation.updateFond({}, { id: 'some-id', input: update }, userContext)).rejects.toThrow(GraphQLError);
+
+      expect(mockUpdate).not.toHaveBeenCalled();
+    });
+
+    it('should throw ZodError if input fails validation', async () => {
+      const invalidUpdateInput = createMockUpdateFondInput({ fondNumber: -1 }); 
+
+      await expect(FondMutation.updateFond({}, { id: 'some-id', input: invalidUpdateInput }, adminContext)).rejects.toThrow(ZodError);
+
+      expect(mockUpdate).not.toHaveBeenCalled();
+    });
+
+    it('should throw custom error msg if fond with this fondNumber is not found', async () => {
+      const update = createMockUpdateFondInput();
+
+      mockUpdate.mockResolvedValue(null);
+
+      await expect(FondMutation.updateFond({}, { id: 'non-existed-id', input: update }, adminContext)).rejects.toThrow(GraphQLError);
+    });
+
+    it('should successfully call repo update method and return updated fond', async () => {
+      const update = createMockUpdateFondInput();
+      const id = '121';
+
+      mockUpdate.mockResolvedValue(update);
+
+      await FondMutation.updateFond({}, { id, input: update }, adminContext);
+
+      expect(mockUpdate).toHaveBeenCalledTimes(1);
+      expect(mockUpdate).toHaveBeenCalledWith(id, update);
+    });
+  });
+
+  describe('deleteFond', () => {
+    it('should throw GraphQLError if user is not an admin', async () => {
+      const deleteId = 'some-id';
+      await expect(FondMutation.deleteFond({}, { id: deleteId }, userContext)).rejects.toThrow(GraphQLError);
+
+      expect(mockDelete).not.toHaveBeenCalled();
+    });
+    it('should successfully call repo delete method and return false - unsuccessfull delete', async () => {
+      const deleteId = 'non-exitested-id';
+      mockDelete.mockResolvedValue(false);
+
+      const isDeleted = await FondMutation.deleteFond({}, { id: deleteId }, adminContext);
+      expect(isDeleted).toBe(false);
+    });
+
+    it('should successfully call repo delete method and return true - successfull delete', async () => {
+      const deleteId = 'some-id';
+      mockDelete.mockResolvedValue(true);
+
+      const isDeleted = await FondMutation.deleteFond({}, { id: deleteId }, adminContext);
+      expect(isDeleted).toBe(true);
+    });
+  });
+});
