1c7754472e454a8aa3d0d126d5c11b6d979d7706
Pavlo Boiko <88787118+LightOrden@users.noreply.github.com>
2026-08-07 11:37:52 +0300

    Remove comments from recalculateFondStats function
    Removed comments explaining the recalculateFondStats function.

--- src/interfaces/graphql/resolvers/case/recalculateFondStats.ts
+++ src/interfaces/graphql/resolvers/case/recalculateFondStats.ts
@@ -7,14 +7,6 @@ type RecalculateFondStatsDeps = {
   fondRepository: IFondRepository;
 };
 
-/**
- * Recalculates and persists the aggregated Case statistics on a Fond document:
- * - casesCount: total number of Cases that belong to the Fond.
- * - descriptionsCount: number of unique descriptionNumber values used by those Cases.
- *
- * Must be called after every Case create/update/delete that could affect the
- * counts of the given Fond (see callers in caseMutation.ts).
- */
 export const recalculateFondStats = async (
   fondId: Case['fondId'],
   { caseRepository, fondRepository }: RecalculateFondStatsDeps
